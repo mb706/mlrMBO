1d0c7a426248fbeac837c5930b05a9d13d1ad5e8
mb706 <things.developer@mb706.com>
2020-11-13 17:39:14 +0100

    adding new parameters / changing objective functions

--- R/multifidelity.R
+++ R/multifidelity.R
@@ -3,6 +3,9 @@
 
 #' @export
 resetOptStateItersTerminator <- function(opt.state, iters, save.on.disk.at) {
+  assertClass(opt.state, "OptState")
+  assertInt(iters)
+  assertIntegerish(save.on.disk.at)
   opt.state$opt.problem$control <- setMBOControlTermination(opt.state$opt.problem$control, iters = iters)
   opt.state$opt.problem$control$save.on.disk.at <- save.on.disk.at
   opt.state$progress <- 0
@@ -10,9 +13,35 @@ resetOptStateItersTerminator <- function(opt.state, iters, save.on.disk.at) {
   opt.state
 }
 
+#' Adds 'default' / NA to new column if new column is added. Removing columns is an error
 #' @export
-resetOptStateParamSet <- function(opt.state, par.set) {
+resetOptStateParamSet <- function(opt.state, par.set, defaults = getDefaults(par.set)) {
+  assertClass(opt.state, "OptState")
+  assertClass(par.set, "ParamSet")
+  assertList(defaults)
+  if (length(defaults)) assertNames(names(defaults), subset.of = getParamIds(par.set))
+
+  op <- opt.state$opt.path
+  ## virgin.op <- makeOptPathDF(par.set = par.set, y.names = op$y.names, minimize = op$minimize,
+  ##   add.transformed.x = op$add.transformed.x, include.error.message = !is.null(op$env$error.message),
+  ##   include.exec.time = !is.null(op$env$exec.time), include.extra = !is.null(op$env$extra))
+  virgin.op <- makeOptPathDF(par.set = par.set, y.names = op$y.names, minimize = op$minimize)
+
+  addOptPathEl(virgin.op,
+    x = insert(lapply(par.set$pars, ParamHelpers:::getParamNA), defaults),
+    y = NA, check.feasible = FALSE)
+  assertSubset(colnames(op$env$path), colnames(virgin.op$env$path))  # error if params are subtracted
+  op$env$path <- insert(virgin.op$env$path[rep(1, nrow(op$env$path)), ], op$env$path)
   opt.state$opt.path$par.set <- par.set
   attributes(opt.state$opt.problem$fun)$par.set <- par.set
+  opt.state
+}
+
+#' @export
+resetOptStateObjective <- function(opt.state, fun, defaults = getDefaults(getParamSet(fun))) {
+  assertClass(opt.state, "OptState")
+  assertClass(fun, "smoof_function")
+  opt.state <- resetOptStateParamSet(opt.state, getParamSet(fun), defaults = defaults)
+  opt.state$opt.problem$fun <- fun
   opt.state
 }
--- todo-files/multifid.R
+++ todo-files/multifid.R
@@ -13,6 +13,8 @@ par.set <- pSS(x: numeric[-4, 4], x2: numeric[-4, 4], x3: numeric[-4, 4], noise:
 par.set2 <- pSS(x: numeric[-4, 4], x2: numeric[-4, 4], x3: numeric[-4, 4], noise: numeric[0.5, 0.5])
 par.set3 <- pSS(x: numeric[-4, 4], x2: numeric[-4, 4], x3: numeric[-4, 4], noise: numeric[0.1, 0.1])
 
+par.set4 <- pSS(x: numeric[-4, 4], x2: numeric[-4, 4], x3: numeric[-4, 4], noise: numeric[0.1, 0.1], extra.x: numeric[1, 2] [[trafo = function(x) -x]])
+
 obj <- makeSingleObjectiveFunction(name = "test",
   fn = function(x) {
     res <- rnorm(1, x$x^2, x$noise)
@@ -25,9 +27,25 @@ obj <- makeSingleObjectiveFunction(name = "test",
   minimize = TRUE
 )
 
+obj2 <- makeSingleObjectiveFunction(name = "test",
+  fn = function(x) {
+    res <- rnorm(1, x$x^2 + x$extra.x, x$noise)
+    attr(res, "extras") <- list(a = 1, .b = list(1, 2, 3))
+    res
+  },
+  par.set = par.set4,
+  noisy = TRUE,
+  has.simple.signature = FALSE,
+  minimize = TRUE
+)
 
-MAXITERATIONS <- 20
-designsize <- 10
+
+
+
+MAXITERATIONS <- 10
+designsize <- 5
+
+unlink("out.rout")
 
 ctrl <- makeMBOControl(
     propose.points = 1,
@@ -38,7 +56,7 @@ ctrl <- makeMBOControl(
     },
     suppress.eval.errors = TRUE,
     save.on.disk.at = seq_len(MAXITERATIONS + 1),
-    save.file.path = "out.rout2",
+    save.file.path = "out.rout",
     on.surrogate.error = "warn") %>%
   setMBOControlInfill(
     crit = makeMBOInfillCritCB(),
@@ -47,8 +65,6 @@ ctrl <- makeMBOControl(
   setMBOControlMultiPoint(method = "cb") %>%
   setMBOControlTermination(iters = MAXITERATIONS)
 
-
-
 mboresult <- mbo(
   fun = obj,
   design = generateRandomDesign(n = designsize, par.set = getParamSet(obj)),
@@ -56,20 +72,33 @@ mboresult <- mbo(
   show.info = TRUE
 )
 
-load("out.rout2")
-opt.state <- resetOptStateItersTerminator(opt.state, 30, 1:31)
+load("out.rout")
+opt.state <- resetOptStateItersTerminator(opt.state, MAXITERATIONS * 2, seq_len(MAXITERATIONS * 3 + 1))
 opt.state <- resetOptStateParamSet(opt.state, par.set2)
 
 mboContinue(opt.state)
 
-load("out.rout2")
-opt.state <- resetOptStateItersTerminator(opt.state, 40, 1:41)
+load("out.rout")
+opt.state <- resetOptStateItersTerminator(opt.state, MAXITERATIONS * 3, seq_len(MAXITERATIONS * 3 + 1))
 opt.state <- resetOptStateParamSet(opt.state, par.set3)
 
 mboContinue(opt.state)
 
+load("out.rout")
+opt.state <- resetOptStateItersTerminator(opt.state, MAXITERATIONS * 5, seq_len(MAXITERATIONS * 5 + 1))
+opt.state <- resetOptStateObjective(opt.state, obj2, defaults = list(extra.x = 0))
+mboContinue(opt.state)
+
+load("out.rout")
+
+
+
 library("ggplot2")
 
-ggplot(as.data.frame(opt.state$opt.path), aes(x = x, y = y, color = log(noise))) + geom_point()
+ggplot(as.data.frame(opt.state$opt.path), aes(x = x, y = y, color = as.factor(noise))) + geom_point()
+ggplot(as.data.frame(opt.state$opt.path), aes(x = x, y = y, color = extra.x)) + geom_point()
+
+ggplot(as.data.frame(opt.state$opt.path), aes(x = extra.x, y = y, color = x)) + geom_point()
+
 ggplot(as.data.frame(opt.state$opt.path), aes(x = x2, y = y, color = log(noise))) + geom_point()
 ggplot(as.data.frame(opt.state$opt.path), aes(x = x3, y = y, color = log(noise))) + geom_point()
